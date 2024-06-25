7f9194da772aa4a9ff7a18528dd2719897d15f82
dbseend <shyoon840@gmail.com>
2024-06-25 22:49:19 +0900

    [FEAT]: COMPLETE LECTURE-DEATIL STUDENT LIST, NEED TO ADD STUDENTS TO LECTURE USING API

--- src/pages/Lecture/LectureDetail.js
+++ src/pages/Lecture/LectureDetail.js
@@ -4,6 +4,7 @@ import { getOneLectureAPI } from "../../API/LectureAPI";
 import styled, { ThemeProvider } from "styled-components";
 import { theme } from "../../style/theme";
 import { ReactComponent as Cancel } from "../../Assets/Cancel.svg";
+import { ReactComponent as Plus } from "../../Assets/Plus.svg";
 import { MainDiv, RowDiv, ColumnDiv } from "../../style/CommonStyle";
 import { getStudentByLectureAPI } from "../../API/StudentAPI";
 import { getAttendanceByLectureAndDateAPI } from "../../API/AttendanceAPI";
@@ -17,9 +18,9 @@ const LectureDetail = () => {
   const [attendances, setAttendances] = useState([]);
   const [assignments, setAssignments] = useState([]);
   const [onClicked, setOnClicked] = useState({
-    student: false,
+    student: true,
     attendance: false,
-    assignment: true,
+    assignment: false,
   });
 
   const taskDummy = [
@@ -94,11 +95,11 @@ const LectureDetail = () => {
     <ThemeProvider theme={theme}>
       <LectureDetailWrapper>
         {/* 수업 정보 */}
+        <TitleWrapper>
+          <Title>{lecture.name}</Title>
+          <X />
+        </TitleWrapper>
         <InfoWrapper>
-          <TitleWrapper>
-            <Title>{lecture.name}</Title>
-            <X />
-          </TitleWrapper>
           <Info>
             <Key>수업시간</Key>
             <Value>
@@ -141,12 +142,18 @@ const LectureDetail = () => {
         {onClicked.student && (
           <ColumnDiv>
             {students.map((student, index) => (
-              <RowDiv key={index}>
-                <div>{student.name}</div>
-                <div>{student.school}</div>
-                <div>{student.grade}</div>
-              </RowDiv>
+              <ColumnDiv key={index}>
+                <StudentWrapper key={index}>
+                  <StudentName>{student.name}</StudentName>
+                  <SchoolAndGrade>
+                    {student.school} l {student.grade}
+                  </SchoolAndGrade>
+                </StudentWrapper>
+                <Line />
+              </ColumnDiv>
             ))}
+            <PlusIcon />
+            <Line />
           </ColumnDiv>
         )}
 
@@ -154,11 +161,14 @@ const LectureDetail = () => {
         {onClicked.attendance && (
           <ColumnDiv>
             {attendances.map((attendance, index) => (
-              <RowDiv key={index}>
-                <div>{attendance.name}</div>
-                <div>{attendance.studentPhoneNumber}</div>
-                <div>{attendance.attendanceType}</div>
-              </RowDiv>
+              <ColumnDiv key={index}>
+                <StudentWrapper key={index}>
+                  <div>{attendance.name}</div>
+                  <div>{attendance.studentPhoneNumber}</div>
+                  <div>{attendance.attendanceType}</div>
+                </StudentWrapper>
+                <Line />
+              </ColumnDiv>
             ))}
           </ColumnDiv>
         )}
@@ -182,15 +192,13 @@ const LectureDetail = () => {
 
 const LectureDetailWrapper = styled(MainDiv)`
   width: 50%;
-  height: 100%;
-  align-items: flex-start;
 `;
 
 const InfoWrapper = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
-  padding-left: 2.5%;
-  width: 100%;
+  padding-left: 12%;
 `;
 
 const Info = styled.div`
@@ -198,28 +206,34 @@ const Info = styled.div`
   margin-bottom: 17px;
 `;
 
-const TitleWrapper = styled.div`
+const TitleWrapper = styled(RowDiv)`
   width: 100%;
+  padding-left: 12%;
 `;
 
 const Title = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
   font-size: ${(props) => props.theme.fontSizes.title2};
   font-weight: 700;
   margin-bottom: 30px;
-  flex: 1;
-  margin-right:45%;
 `;
 
 const X = styled(Cancel)`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 13%;
   width: 16.5px;
   height: 18px;
-  cursor: pointer; /* 커서 포인터로 변경 */
+  cursor: pointer;
 `;
 
 const Key = styled.div`
   font-size: ${(props) => props.theme.fontSizes.bodyText3};
   font-weight: 700;
-  margin-right: 10px; /* Add margin to separate key and value */
+  margin-right: 10px;
 `;
 
 const Value = styled.div`
@@ -231,14 +245,15 @@ const ButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
+  width: 90%;
   height: 50px;
   border-radius: 10px;
+  margin-bottom: 15px;
   background-color: ${(props) => props.theme.colors.gray_001};
 `;
 
 const Button = styled.button`
-  width: 31%;
+  width: 33.3%;
   height: 100%;
   border-radius: 5px;
   margin: 0 3px;
@@ -259,6 +274,37 @@ const Button = styled.button`
   &:hover {
     transform: translateY(-2px);
   }
+`;
+
+const StudentWrapper = styled(RowDiv)`
+  justify-content: flex-start;
+  padding-left: 8%;
+  margin: 9px 0;
+`;
+const StudentName = styled.div`
+  font-size: ${(props) => props.theme.fontSizes.bodyText3};
+  font-weight: 700;
+  padding-right: 18px;
+`;
+
+const SchoolAndGrade = styled.div`
+  font-size: ${(props) => props.theme.fontSizes.bodyText4};
+  font-weight: 400;
+`;
+
+const Line = styled.hr`
+  width: 90%;
+  border: none;
+  height: 1px;
+  background-color: ${(props) => props.theme.colors.gray_003};
+`;
+
+const PlusIcon = styled(Plus)`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
+  margin: 9px 0;
 `;
 
 export default LectureDetail;
