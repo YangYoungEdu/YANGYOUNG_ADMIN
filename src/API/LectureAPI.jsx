0442e86c609b5ec3be92fe8b0612f9baf0e6e73b
dbseend <shyoon840@gmail.com>
2024-07-29 02:35:22 +0900

    fix postLecture request Body location

--- src/API/LectureAPI.jsx
+++ src/API/LectureAPI.jsx
@@ -21,7 +21,7 @@ export const getAllLectureByMonthAPI = async (date) => {
     console.log(response.data);
     return response.data;
   } catch (error) {
-    if(error.response.status === 403){
+    if (error.response.status === 403) {
       alert("로그인 후 이용해주세요.");
       window.location.href = "/";
     }
@@ -46,7 +46,7 @@ export const getAllLectureByWeekAPI = async (date) => {
     console.log(response.data);
     return response.data;
   } catch (error) {
-    if(error.response.status === 403){
+    if (error.response.status === 403) {
       alert("로그인 후 이용해주세요.");
       window.location.href = "/";
     }
@@ -90,7 +90,7 @@ export const getLectureByStudentAPI = async (studentId) => {
     console.log(response.data);
     return response.data;
   } catch (error) {
-    if(error.response.status === 403){
+    if (error.response.status === 403) {
       alert("로그인 후 이용해주세요.");
       window.location.href = "/";
     }
@@ -109,7 +109,7 @@ export const getOneLectureAPI = async (lectureId) => {
     console.log(response.data);
     return response.data;
   } catch (error) {
-    if(error.response.status === 403){
+    if (error.response.status === 403) {
       alert("로그인 후 이용해주세요.");
       window.location.href = "/";
     }
@@ -120,16 +120,15 @@ export const getOneLectureAPI = async (lectureId) => {
 // 강의 정보 등록 폼
 export const postLecture = async (data) => {
   try {
-    const response = await axios.post(`${server}lecture`, {
+    const response = await axios.post(`${server}lecture`, data, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
       },
-      data
     });
     console.log(response.data);
     return response.data;
   } catch (error) {
-    if(error.response.status === 403){
+    if (error.response.status === 403) {
       alert("로그인 후 이용해주세요.");
       window.location.href = "/";
     }
@@ -151,7 +150,7 @@ export const deleteLecture = async (lectureIds) => {
     console.log(response.data);
     return response.data;
   } catch (error) {
-    if(error.response.status === 403){
+    if (error.response.status === 403) {
       alert("로그인 후 이용해주세요.");
       window.location.href = "/";
     }
@@ -166,12 +165,12 @@ export const patchLecture = async (data) => {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
       },
-      data
+      data,
     });
     console.log(response.data);
     return response.data;
   } catch (error) {
-    if(error.response.status === 403){
+    if (error.response.status === 403) {
       alert("로그인 후 이용해주세요.");
       window.location.href = "/";
     }
