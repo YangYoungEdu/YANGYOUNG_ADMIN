5e0c846a2a730adc61eb8319b50f426bf94acf97
KKIMYENN <yenny031003@gmail.com>
2024-07-30 00:46:23 +0900

    예은: 강의 상세 모달 세팅

---
+++ src/components/Lecture/CalendarModal/LectureAttendance.jsx
@@ -0,0 +1,238 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import {
+  getAttendanceByLectureAndDateAPI,
+  updateAttendanceAPI,
+} from "../../../API/AttendanceAPI";
+import { RowDiv } from "../../../style/CommonStyle";
+
+const LectureAttendance = () => {
+  const [attendances, setAttendances] = useState([]);
+  const [isUpdated, setIsUpdated] = useState(false);
+
+  // useEffect(() => {
+  //   getAttendanceByLectureAndDateAPI(id, date).then((res) => {
+  //     setAttendances(res);
+  //   });
+  // }, [isUpdated]);
+
+  // const updateAttendance = async () => {
+  //   console.log(attendances);
+
+  //   const updateRequest = attendances
+  //     .filter((attendance) => attendance.attendanceType !== null) // Filter out entries where attendanceType is null
+  //     .map((attendance) => ({
+  //       id: attendance.id,
+  //       studentId: attendance.studentId,
+  //       lectureId: id,
+  //       attendanceType: attendance.attendanceType,
+  //     }));
+
+  //   try {
+  //     await updateAttendanceAPI(updateRequest);
+  //     setIsUpdated(true);
+  //     alert("출석 정보가 변경되었습니다.");
+  //   } catch (error) {
+  //     console.error("Error updating attendance:", error);
+  //   }
+  // };
+
+  const handleOptionChange = (e, studentId) => {
+    const { value } = e.target;
+    const updatedAttendances = attendances.map((attendance) =>
+      attendance.studentId === studentId
+        ? { ...attendance, attendanceType: value }
+        : attendance
+    );
+    setAttendances(updatedAttendances);
+  };
+
+  return (
+    <TableWrapper>
+      <AttendanceTable>
+        <thead>
+          <tr>
+            <TableHeader style={{ width: "164px" }}>이름</TableHeader>
+            <TableHeader style={{ width: "174px" }}>학생 연락처</TableHeader>
+            <TableHeader>출결</TableHeader>
+          </tr>
+        </thead>
+        <tbody>
+          {attendances.map((attendance, index) => (
+            <tr key={index}>
+              <TableCell>{attendance.name}</TableCell>
+              <TableCell>{attendance.studentPhoneNumber}</TableCell>
+              <TableCell>
+                <RadioWrapper>
+                  <label>
+                    <RadioInput
+                      type="radio"
+                      name={`attendance-${attendance.studentId}`} // 고유한 name 속성 부여
+                      value="ATTENDANCE"
+                      checked={attendance.attendanceType === "ATTENDANCE"}
+                      onChange={(e) =>
+                        handleOptionChange(e, attendance.studentId)
+                      }
+                    />
+                    출석
+                  </label>
+                  <label>
+                    <RadioInput
+                      type="radio"
+                      name={`attendance-${attendance.studentId}`} // 고유한 name 속성 부여
+                      value="LATE"
+                      checked={attendance.attendanceType === "LATE"}
+                      onChange={(e) =>
+                        handleOptionChange(e, attendance.studentId)
+                      }
+                    />
+                    지각
+                  </label>
+                  <label>
+                    <RadioInput
+                      type="radio"
+                      name={`attendance-${attendance.studentId}`} // 고유한 name 속성 부여
+                      value="ABSENCE"
+                      checked={attendance.attendanceType === "ABSENCE"}
+                      onChange={(e) =>
+                        handleOptionChange(e, attendance.studentId)
+                      }
+                    />
+                    결석
+                  </label>
+                </RadioWrapper>
+              </TableCell>
+            </tr>
+          ))}
+        </tbody>
+      </AttendanceTable>
+      {/* <UploadButton onClick={updateAttendance}>
+        저장 사항 변경
+      </UploadButton> */}
+    </TableWrapper>
+  );
+};
+
+const TableWrapper = styled.div`
+  width: 100%;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Table = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0;
+  border-radius: 10px;
+  margin-bottom: 20px;
+  /* overflow: hidden; */
+`;
+
+const TableHeader = styled.th`
+  box-sizing: border-box;
+  width: 176px;
+  height: 48px;
+  padding: 10px;
+  text-align: center;
+  background-color: ${(props) => props.theme.colors.gray_001};
+  &:first-child {
+    border-top-left-radius: 10px;
+  }
+
+  &:last-child {
+    border-top-right-radius: 10px;
+  }
+`;
+
+const TableCell = styled.td`
+  box-sizing: border-box;
+  width: 176px;
+  height: 50px;
+  border: 1px solid;
+  border-color: ${(props) => props.theme.colors.gray_002};
+  padding: 8px;
+  text-align: center;
+`;
+
+const AttendanceTable = styled(Table)`
+  tbody tr:last-child ${TableCell}:first-child {
+    border-bottom-left-radius: 10px;
+  }
+
+  tbody tr:last-child ${TableCell}:last-child {
+    border-bottom-right-radius: 10px;
+  }
+`;
+
+const RadioWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const RadioInput = styled.input.attrs({ type: "radio" })`
+  width: 12px;
+  height: 12px;
+  border: 0.5px solid #95c25c;
+  border-radius: 50%;
+  outline: none;
+  margin-left: 16px;
+  margin-right: 3px;
+  position: relative;
+  cursor: pointer;
+
+  &::before {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: white;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  &::after {
+    content: "";
+    display: block;
+    width: 60%;
+    height: 60%;
+    border-radius: 50%;
+    background-color: #95c25c;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    opacity: 0;
+  }
+
+  &:checked::after {
+    opacity: 1;
+  }
+`;
+
+const TaskBox = styled(RowDiv)`
+  border-radius: 5px;
+  border: 1px solid #e0e0e0;
+  box-sizing: border-box;
+`;
+
+const UploadButton = styled(TaskBox)`
+  box-sizing: border-box;
+  background-color: #95c25c;
+
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 10px;
+  margin-top: 10px;
+  width: 50%;
+
+  color: white;
+  font-size: 20px;
+`;
+
+export default LectureAttendance;
--- src/components/Lecture/CalendarModal/ModalDesign.jsx
+++ src/components/Lecture/CalendarModal/ModalDesign.jsx
@@ -9,6 +9,11 @@ import MultiDatePicker from "../CalendarDetail/MultiDatePicker.jsx";
 import AddStudentSearch from "../CalendarDetail/AddStudentSearch.jsx";
 import AddGenericTable from "../CalendarDetail/AddGenericTable.jsx";
 
+import LectureStudent from "./LectureStudent.jsx";
+import LectureAttendance from "./LectureAttendance.jsx";
+import LectureTask from "./LectureTask.jsx";
+import LectureMaterial from "./LectureMaterial.jsx";
+
 import { ReactComponent as LightX } from "../../../Assets/LightX.svg";
 
 //보여주는 모달 디자인
@@ -35,6 +40,23 @@ const ModalDesign = ({
 
   //edit
 }) => {
+  const [onClicked, setOnClicked] = useState({
+    student: true,
+    attendance: false,
+    task: false,
+    material: false,
+  });
+
+  const handleButtonClick = (type) => {
+    setOnClicked((prevState) => ({
+      ...prevState,
+      student: type === "student" ? true : false,
+      attendance: type === "attendance" ? true : false,
+      task: type === "task" ? true : false,
+      material: type === "material" ? true : false,
+    }));
+  };
+
   return (
     <Panel id="panel">
       <StyledAddForm id="add-form">
@@ -71,44 +93,9 @@ const ModalDesign = ({
             </ButtonContainer>
           </UpperDiv>
 
-          {/* 수업시간, 선생님, 강의실, 강의타입 */}
-          <div id="lectureType-picker-form">
-            <div className="label">강의타입</div>
-            <div>
-              <select
-                id="lectureType-select"
-                value={mode === "add" ? newAddFormState.lectureType : null}
-                onChange={onChangeNewAddFormState}
-              >
-                <option value="일반">일반</option>
-                <option value="특반">특반</option>
-              </select>
-            </div>
-          </div>
-          <div id="teacher-picker-form">
-            <div className="label">담당 선생님</div>
-            <div>
-              <select
-                id="teacher-select"
-                value={mode === "add" ? newAddFormState.teacher : null}
-                onChange={onChangeNewAddFormState}
-              >
-                <option value="">선택하세요</option>
-                <option value="김삼유">김삼유</option>
-                <option value="장영해">장영해</option>
-                <option value="전재우">전재우</option>
-              </select>
-            </div>
-          </div>
-          <div id="input-form">
-            <div className="label">강의실</div>
-            <input
-              id="input-room"
-              value={mode === "add" ? newAddFormState.room : null}
-              onChange={onChangeNewAddFormState}
-            />
-          </div>
-          <div id="date-picker-form">
+          <MiddleDiv>
+            {/* datepicker 먼저 */}
+            {/* <div id="date-picker-form">
             <div id="date-picker">
               <MultiDatePicker
                 multidates={multidates}
@@ -116,65 +103,149 @@ const ModalDesign = ({
                 curDate={mode === "add" ? newAddFormState.curDate : null}
               />
             </div>
-          </div>
-          <div id="time-picker-form">
-            <div className="label">시작 시간</div>
-            <div>
-              <select
-                id="start-hour"
-                value={mode === "add" ? newAddFormState.startTime.hour : null}
+          </div> */}
+
+            {/* 수업시간, 선생님, 강의실, 강의타입 */}
+            <DetailInfo id="time-picker-form">
+              <Label className="label">수업 시간</Label>
+              <TimePicker>
+                <select
+                  id="start-hour"
+                  value={mode === "add" ? newAddFormState.startTime.hour : null}
+                  onChange={onChangeNewAddFormState}
+                >
+                  {Array.from({ length: 24 }, (_, i) => (
+                    <option key={i} value={i}>
+                      {i}
+                    </option>
+                  ))}
+                </select>
+                시
+                <select
+                  id="start-minute"
+                  value={
+                    mode === "add" ? newAddFormState.startTime.minute : null
+                  }
+                  onChange={onChangeNewAddFormState}
+                >
+                  {Array.from({ length: 60 }, (_, i) => (
+                    <option key={i} value={i}>
+                      {i}
+                    </option>
+                  ))}
+                </select>
+                분 ~
+                <select
+                  id="end-hour"
+                  value={mode === "add" ? newAddFormState.endTime.hour : null}
+                  onChange={onChangeNewAddFormState}
+                >
+                  {Array.from({ length: 24 }, (_, i) => (
+                    <option key={i} value={i}>
+                      {i}
+                    </option>
+                  ))}
+                </select>
+                시
+                <select
+                  id="end-minute"
+                  value={mode === "add" ? newAddFormState.endTime.minute : null}
+                  onChange={onChangeNewAddFormState}
+                >
+                  {Array.from({ length: 60 }, (_, i) => (
+                    <option key={i} value={i}>
+                      {i}
+                    </option>
+                  ))}
+                </select>
+                분
+              </TimePicker>
+            </DetailInfo>
+
+            <DetailInfo id="lectureType-picker-form">
+              <Label className="label">강의타입</Label>
+              <SelectWrapper>
+                <select
+                  id="lectureType-select"
+                  value={mode === "add" ? newAddFormState.lectureType : null}
+                  onChange={onChangeNewAddFormState}
+                >
+                  <option value="일반">일반</option>
+                  <option value="특반">특강</option>
+                </select>
+              </SelectWrapper>
+            </DetailInfo>
+            <DetailInfo id="teacher-picker-form">
+              <Label className="label">담당 선생님</Label>
+              <SelectWrapper>
+                <select
+                  id="teacher-select"
+                  value={mode === "add" ? newAddFormState.teacher : null}
+                  onChange={onChangeNewAddFormState}
+                >
+                  <option value="">선택하세요</option>
+                  <option value="김삼유">김삼유</option>
+                  <option value="장영해">장영해</option>
+                  <option value="전재우">전재우</option>
+                </select>
+              </SelectWrapper>
+            </DetailInfo>
+            <DetailInfo id="input-form">
+              <Label className="label">강의실</Label>
+              <StyledMiddleInput
+                id="input-room"
+                value={mode === "add" ? newAddFormState.room : null}
                 onChange={onChangeNewAddFormState}
+                placeholder="강의실을 적어주세요."
+              />
+            </DetailInfo>
+          </MiddleDiv>
+
+          <LowerDiv>
+            {/* 학생, 출석, 과제 버튼*/}
+            <ButtonWrapper>
+              <Button
+                isActive={onClicked.student}
+                onClick={() => handleButtonClick("student")}
               >
-                {Array.from({ length: 24 }, (_, i) => (
-                  <option key={i} value={i}>
-                    {i}
-                  </option>
-                ))}
-              </select>
-              시
-              <select
-                id="start-minute"
-                value={mode === "add" ? newAddFormState.startTime.minute : null}
-                onChange={onChangeNewAddFormState}
+                학생
+              </Button>
+              <Button
+                isActive={onClicked.attendance}
+                onClick={() => handleButtonClick("attendance")}
               >
-                {Array.from({ length: 60 }, (_, i) => (
-                  <option key={i} value={i}>
-                    {i}
-                  </option>
-                ))}
-              </select>
-              분
-            </div>
-          </div>
-          <div id="time-picker-form">
-            <div className="label">종료 시간</div>
-            <div>
-              <select
-                id="end-hour"
-                value={mode === "add" ? newAddFormState.endTime.hour : null}
-                onChange={onChangeNewAddFormState}
+                출석 체크
+              </Button>
+              <Button
+                isActive={onClicked.task}
+                onClick={() => handleButtonClick("task")}
               >
-                {Array.from({ length: 24 }, (_, i) => (
-                  <option key={i} value={i}>
-                    {i}
-                  </option>
-                ))}
-              </select>
-              시
-              <select
-                id="end-minute"
-                value={mode === "add" ? newAddFormState.endTime.minute : null}
-                onChange={onChangeNewAddFormState}
+                과제
+              </Button>
+              <Button
+                isActive={onClicked.material}
+                onClick={() => handleButtonClick("material")}
               >
-                {Array.from({ length: 60 }, (_, i) => (
-                  <option key={i} value={i}>
-                    {i}
-                  </option>
-                ))}
-              </select>
-              분
-            </div>
-          </div>
+                {/* {" "} */}
+                수업 자료
+              </Button>
+            </ButtonWrapper>
+
+            {/* ToDo: 조건에 따라 정보 표시 */}
+            {/* 강의별 학생 목록*/}
+            {onClicked.student && <LectureStudent />}
+
+            {/* 강의별 출석 목록*/}
+            {onClicked.attendance && <LectureAttendance />}
+
+            {/* 강의별 과제 목록*/}
+            {onClicked.task && <LectureTask />}
+
+            {/* 강의별 자료 목록 */}
+            {/* ToDo: 강의별 자료 목록 API 연동 */}
+            {onClicked.material && <LectureMaterial />}
+          </LowerDiv>
+
           {/* <div id="select-student"> */}
           {/* 검색 영역  */}
           {/* {mode === "add" ? (
@@ -219,8 +290,8 @@ const StyledAddForm = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  padding: 10px;
-  /* padding: 29px 46px 29px 46px; */
+
+  padding: 29px 46px 29px 46px;
   box-sizing: border-box;
 
   position: fixed;
@@ -234,7 +305,6 @@ const StyledAddForm = styled.div`
 `;
 
 const TopDiv = styled.div`
-  background-color: aliceblue;
   width: 100%;
   height: 100%;
 `;
@@ -244,7 +314,9 @@ const UpperDiv = styled.div`
   flex-direction: row;
   justify-content: space-between;
   flex-wrap: wrap;
+
   width: 100%;
+  margin-bottom: 33px;
   input {
     font-family: Pretendard Variable;
     font-size: 26px;
@@ -275,4 +347,94 @@ const SubmitButton = styled.button`
   cursor: pointer;
 `;
 
+const MiddleDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 17px;
+`;
+
+const DetailInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 22px;
+`;
+
+const Label = styled.label`
+  font-family: Pretendard Variable;
+  font-size: 14px;
+  font-weight: 700;
+  text-align: left;
+`;
+
+const StyledMiddleInput = styled.input`
+  font-family: Pretendard Variable;
+  font-size: 14px;
+  font-weight: 400;
+  text-align: left;
+
+  &::placeholder {
+    color: #bababa;
+  }
+`;
+const TimePicker = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  font-family: Pretendard Variable;
+  font-size: 14px;
+  font-weight: 400;
+  text-align: left;
+`;
+
+const SelectWrapper = styled.div`
+  font-family: Pretendard Variable;
+  font-size: 14px;
+  font-weight: 400;
+  text-align: left;
+  cursor: pointer;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  border-radius: 10px;
+  margin-bottom: 15px;
+  background-color: ${(props) => props.theme.colors.gray_001};
+`;
+
+const Button = styled.button`
+  width: 25%;
+  height: 37px;
+  border-radius: 5px;
+  margin: 0 3px;
+  font-size: ${(props) => props.theme.fontSizes.bodyText3};
+  text-align: center;
+  font-weight: 700;
+  cursor: pointer;
+
+  background-color: ${(props) =>
+    props.isActive ? "#95C25C" : props.theme.colors.gray_001};
+  color: ${(props) =>
+    props.isActive ? props.theme.colors.white : props.theme.colors.black};
+
+  border: none;
+  outline: none;
+  transition: all 0.3s ease;
+
+  &:hover {
+    transform: translateY(-2px);
+  }
+`;
+
+const LowerDiv = styled.div`
+  margin-top: 42px;
+  width: 100%;
+`;
 export default ModalDesign;
