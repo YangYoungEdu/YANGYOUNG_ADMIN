4f332fe1a636bdfa48a47eb79eff3ce6dce9ac5b
KKIMYENN <yenny031003@gmail.com>
2024-07-29 23:34:25 +0900

    예은: 상세 모달 구조 잡기 및 일부 디테일

--- src/components/Lecture/CalendarModal/ModalDesign.jsx
+++ src/components/Lecture/CalendarModal/ModalDesign.jsx
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import styled from 'styled-components';
+import React, { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import styled from "styled-components";
 
-import { useAddFormState } from '../../../stores/addFormState.jsx';
-import AddForm from './AddForm.jsx';
-import MultiDatePicker from '../CalendarDetail/MultiDatePicker.jsx';
-import AddStudentSearch from '../CalendarDetail/AddStudentSearch.jsx';
-import AddGenericTable from '../CalendarDetail/AddGenericTable.jsx';
+import { useAddFormState } from "../../../stores/addFormState.jsx";
+import AddForm from "./AddForm.jsx";
+import MultiDatePicker from "../CalendarDetail/MultiDatePicker.jsx";
+import AddStudentSearch from "../CalendarDetail/AddStudentSearch.jsx";
+import AddGenericTable from "../CalendarDetail/AddGenericTable.jsx";
+
+import { ReactComponent as LightX } from "../../../Assets/LightX.svg";
 
 //보여주는 모달 디자인
 const ModalDesign = ({
@@ -33,97 +35,157 @@ const ModalDesign = ({
 
   //edit
 }) => {
-
   return (
-    <div id="panel">
-      <div id="add-form">
-        <div id="add-form-title">{mode === 'add' ? '일정 추가' : '일정 수정'}</div>
-        <div id="input-form">
-          <div className="label">이름</div>
-          <input id="input-name" value={mode === 'add' ?newAddFormState.name: null} onChange={onChangeNewAddFormState} />
-        </div>
-        <div id="lectureType-picker-form">
-          <div className="label">강의타입</div>
-          <div>
-            <select id="lectureType-select" value={mode ==='add' ? newAddFormState.lectureType: null} onChange={onChangeNewAddFormState} >
-            <option value="일반">일반</option>
-            <option value="특반">특반</option>
-            </select>
+    <Panel id="panel">
+      <StyledAddForm id="add-form">
+        <TopDiv>
+          {/* 강의제목, 저장 버튼, 닫기 버튼 */}
+          <UpperDiv id="input-form">
+            {/* <div className="label">이름</div> */}
+            <input
+              id="input-name"
+              value={mode === "add" ? newAddFormState.name : null}
+              onChange={onChangeNewAddFormState}
+              placeholder="수업 이름을 적어주세요."
+            />
+
+            <ButtonContainer id="option-form">
+              {/* 저장, 수정 버튼 */}
+              {mode === "add" ? (
+                <SubmitButton id="add-btn" className="btn" onClick={onClickAdd}>
+                  저장
+                </SubmitButton>
+              ) : null}
+              {mode === "edit" ? (
+                <>
+                  <SubmitButton id="edit-btn" className="btn">
+                    수정
+                  </SubmitButton>
+                  <SubmitButton id="delete-btn" className="btn">
+                    삭제
+                  </SubmitButton>
+                </>
+              ) : null}
+
+              <LightX id="cancel-btn" className="btn" onClick={onClickCancel} />
+            </ButtonContainer>
+          </UpperDiv>
+
+          {/* 수업시간, 선생님, 강의실, 강의타입 */}
+          <div id="lectureType-picker-form">
+            <div className="label">강의타입</div>
+            <div>
+              <select
+                id="lectureType-select"
+                value={mode === "add" ? newAddFormState.lectureType : null}
+                onChange={onChangeNewAddFormState}
+              >
+                <option value="일반">일반</option>
+                <option value="특반">특반</option>
+              </select>
+            </div>
           </div>
-        </div>
-        <div id="teacher-picker-form">
-          <div className="label">담당 선생님</div>
-          <div>
-            <select id="teacher-select" value={mode==='add'?newAddFormState.teacher: null} onChange={onChangeNewAddFormState}>
-              <option value="">선택하세요</option>
-              <option value="김삼유">김삼유</option>
-              <option value="장영해">장영해</option>
-              <option value="전재우">전재우</option>
-            </select>
+          <div id="teacher-picker-form">
+            <div className="label">담당 선생님</div>
+            <div>
+              <select
+                id="teacher-select"
+                value={mode === "add" ? newAddFormState.teacher : null}
+                onChange={onChangeNewAddFormState}
+              >
+                <option value="">선택하세요</option>
+                <option value="김삼유">김삼유</option>
+                <option value="장영해">장영해</option>
+                <option value="전재우">전재우</option>
+              </select>
+            </div>
           </div>
-        </div>
-        <div id="input-form">
-          <div className="label">강의실</div>
-          <input id="input-room" value={mode==='add' ?newAddFormState.room: null} onChange={onChangeNewAddFormState} />
-        </div>
-        <div id="date-picker-form">
-          <div id="date-picker">
-            <MultiDatePicker 
-              multidates={multidates}
-              setmultiDates={setmultiDates} 
-              curDate={mode ==='add'? newAddFormState.curDate: null}/>
+          <div id="input-form">
+            <div className="label">강의실</div>
+            <input
+              id="input-room"
+              value={mode === "add" ? newAddFormState.room : null}
+              onChange={onChangeNewAddFormState}
+            />
           </div>
-        </div>
-        <div id="time-picker-form">
-          <div className="label">시작 시간</div>
-          <div>
-            <select id="start-hour" value={mode ==='add'?newAddFormState.startTime.hour: null} onChange={onChangeNewAddFormState}>
-              {Array.from({ length: 24 }, (_, i) => (
-                <option key={i} value={i}>
-                  {i}
-                </option>
-              ))}
-            </select>
-            시
-            <select id="start-minute" value={mode ==='add'?newAddFormState.startTime.minute: null} onChange={onChangeNewAddFormState}>
-              {Array.from({ length: 60 }, (_, i) => (
-                <option key={i} value={i}>
-                  {i}
-                </option>
-              ))}
-            </select>
-            분
+          <div id="date-picker-form">
+            <div id="date-picker">
+              <MultiDatePicker
+                multidates={multidates}
+                setmultiDates={setmultiDates}
+                curDate={mode === "add" ? newAddFormState.curDate : null}
+              />
+            </div>
           </div>
-        </div>
-        <div id="time-picker-form">
-          <div className="label">종료 시간</div>
-          <div>
-            <select id="end-hour" value={mode ==='add'?newAddFormState.endTime.hour: null} onChange={onChangeNewAddFormState}>
-              {Array.from({ length: 24 }, (_, i) => (
-                <option key={i} value={i}>
-                  {i}
-                </option>
-              ))}
-            </select>
-            시
-            <select id="end-minute" value={mode ==='add'?newAddFormState.endTime.minute: null} onChange={onChangeNewAddFormState}>
-              {Array.from({ length: 60 }, (_, i) => (
-                <option key={i} value={i}>
-                  {i}
-                </option>
-              ))}
-            </select>
-            분
+          <div id="time-picker-form">
+            <div className="label">시작 시간</div>
+            <div>
+              <select
+                id="start-hour"
+                value={mode === "add" ? newAddFormState.startTime.hour : null}
+                onChange={onChangeNewAddFormState}
+              >
+                {Array.from({ length: 24 }, (_, i) => (
+                  <option key={i} value={i}>
+                    {i}
+                  </option>
+                ))}
+              </select>
+              시
+              <select
+                id="start-minute"
+                value={mode === "add" ? newAddFormState.startTime.minute : null}
+                onChange={onChangeNewAddFormState}
+              >
+                {Array.from({ length: 60 }, (_, i) => (
+                  <option key={i} value={i}>
+                    {i}
+                  </option>
+                ))}
+              </select>
+              분
+            </div>
           </div>
-        </div>
-        <div id= 'select-student'>
+          <div id="time-picker-form">
+            <div className="label">종료 시간</div>
+            <div>
+              <select
+                id="end-hour"
+                value={mode === "add" ? newAddFormState.endTime.hour : null}
+                onChange={onChangeNewAddFormState}
+              >
+                {Array.from({ length: 24 }, (_, i) => (
+                  <option key={i} value={i}>
+                    {i}
+                  </option>
+                ))}
+              </select>
+              시
+              <select
+                id="end-minute"
+                value={mode === "add" ? newAddFormState.endTime.minute : null}
+                onChange={onChangeNewAddFormState}
+              >
+                {Array.from({ length: 60 }, (_, i) => (
+                  <option key={i} value={i}>
+                    {i}
+                  </option>
+                ))}
+              </select>
+              분
+            </div>
+          </div>
+          {/* <div id="select-student"> */}
           {/* 검색 영역  */}
-          {mode==='add'? <AddStudentSearch
+          {/* {mode === "add" ? (
+            <AddStudentSearch
               searchKeyword={searchKeyword}
               setSearchKeyword={setSearchKeyword}
-          /> :null}
+            />
+          ) : null} */}
           {/* 테이블  */}
-            {mode ==='add' ?<AddGenericTable
+          {/* {mode === "add" ? (
+            <AddGenericTable
               searchData={searchData}
               setSearchData={setSearchData}
               searchDataCount={searchDataCount}
@@ -133,31 +195,84 @@ const ModalDesign = ({
               selectedStudent={selectedStudent}
               active={active}
               setSelectedStudent={setSelectedStudent}
-            /> :null}
-        </div>
-        <div id="option-form">
-          <div id="cancel-btn" className="btn" onClick={onClickCancel}>
-            취소
-          </div>
-          {mode === 'add' ? (
-            <div id="add-btn" className="btn" onClick={onClickAdd}>
-              저장
-            </div>
+            />
           ) : null}
-          {mode === 'edit' ? (
-            <>
-              <div id="edit-btn" className="btn" >
-                수정
-              </div>
-              <div id="delete-btn" className="btn" >
-                삭제
-              </div>
-            </>
-          ) : null}
-        </div>
-      </div>
-    </div>
+        </div> */}
+        </TopDiv>
+      </StyledAddForm>
+    </Panel>
   );
 };
+
+const Panel = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 50;
+`;
+
+const StyledAddForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 10px;
+  /* padding: 29px 46px 29px 46px; */
+  box-sizing: border-box;
+
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+
+  width: 50%;
+
+  background-color: white;
+`;
+
+const TopDiv = styled.div`
+  background-color: aliceblue;
+  width: 100%;
+  height: 100%;
+`;
+
+const UpperDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 100%;
+  input {
+    font-family: Pretendard Variable;
+    font-size: 26px;
+    font-weight: 700;
+    color: #555;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 24px;
+`;
+
+const SubmitButton = styled.button`
+  width: 60px;
+  height: 31px;
+  border-radius: 5px;
+  background-color: #efefef;
+
+  text-align: center;
+  font-family: Pretendard Variable;
+  font-size: 14px;
+  font-weight: 400;
+  color: #555555;
+  white-space: nowrap;
+  cursor: pointer;
+`;
 
 export default ModalDesign;
